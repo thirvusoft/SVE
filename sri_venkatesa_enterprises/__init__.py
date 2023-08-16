2d9810eb97f23f2c99e3f46022947f3a59ec844d
Shankarv19bcr <shankar@thirvusoft.in>
2023-08-16 17:06:51 +0530

    add sales person tab in sales order

--- sri_venkatesa_enterprises/__init__.py
+++ sri_venkatesa_enterprises/__init__.py
@@ -1,8 +1,8 @@
 
 __version__ = '0.0.1'
 
-# from erpnext.controllers.selling_controller import SellingController
+from erpnext.controllers.selling_controller import SellingController
 
-# from sri_venkatesa_enterprises.sri_venkatesa_enterprisescustom.py.sales_invoice import sales_contribution
+from sri_venkatesa_enterprises.sri_venkatesa_enterprises.custom.py.sales_invoice import sales_contribution
 
-# SellingController.calculate_contribution = sales_contribution
+SellingController.calculate_contribution = sales_contribution
