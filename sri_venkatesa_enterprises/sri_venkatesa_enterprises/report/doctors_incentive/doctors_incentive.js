952df8769a3b5e0b0024e5b18ad7bd2a1c73815b
Parameshwari Palanisamy <101092028+creative-paramu@users.noreply.github.com>
2023-11-21 09:53:06 +0530

    Update doctors_incentive.js

--- sri_venkatesa_enterprises/sri_venkatesa_enterprises/report/doctors_incentive/doctors_incentive.js
+++ sri_venkatesa_enterprises/sri_venkatesa_enterprises/report/doctors_incentive/doctors_incentive.js
@@ -23,11 +23,15 @@ frappe.query_reports["Doctors Incentive"] = {
 			"fieldname":"from_date",
 			"label":"From Date",
 			"fieldtype":"Date",
+			"default":frappe.datetime.month_start(),
+			"reqd":1
 		},
 		{
 			"fieldname":"to_date",
 			"label":"To Date",
 			"fieldtype":"Date",
+			"default":frappe.datetime.month_end(),
+			"reqd":1
 		},
 		{
 			"fieldname":"item_group",
